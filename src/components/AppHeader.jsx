07c1cd795ff1ea211c3ed0737e4921b8c24aec57
AlesciDavide <davi.alesci@gmail.com>
2024-11-11 16:20:26 +0100

    creazione componenti

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+export default function AppHeader(){
+
+    const title = 'Il mio header react';
+
+    return(
+        <header>
+            <h1>{title.toUpperCase()}</h1>
+        </header>
+    )
+}
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,7 @@
+export default function AppMain(){
+
+
+    return(
+        <main>Il mio main</main>
+    )
+}
